8907d3f39d9c29fa32ee6e8e1342a9767bd1cb1d
Louai <louai1998@gmail.com>
2019-05-13 10:28:03 +0100

    Last change

--- app/src/main/java/com/example/softwareproject/Profile.java
+++ app/src/main/java/com/example/softwareproject/Profile.java
@@ -141,10 +141,15 @@ public class Profile extends AppCompatActivity {
         }
         // Reference to an image file in Firebase Storage
         StorageReference storageReference = FirebaseStorage.getInstance().getReference("documentImages/" + email);
-        Toast.makeText(this,email, Toast.LENGTH_LONG).show();
+
+
 
         try {
             final File localFile = File.createTempFile("images", "jpg");
+            if (localFile == null){
+                Toast.makeText(this,"No pic", Toast.LENGTH_SHORT).show();
+            }
+
             storageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
