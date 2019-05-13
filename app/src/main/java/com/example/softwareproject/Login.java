783e21ea6f5ec48497a1d003c7fbcf62f850c560
Reuben <rs00857@surrey.ac.uk>
2019-05-13 13:18:57 +0100

    Added GDPR notice

--- app/src/main/java/com/example/softwareproject/Login.java
+++ app/src/main/java/com/example/softwareproject/Login.java
@@ -1,9 +1,11 @@
 package com.example.softwareproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -30,6 +32,27 @@ public class Login extends AppCompatActivity {
         inputEmail = findViewById(R.id.input_username);
         inputPassword = findViewById(R.id.input_passwordFinal);
         auth = FirebaseAuth.getInstance();
+
+        new AlertDialog.Builder(Login.this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Security Confirmation")
+                .setMessage("Simply Fit needs to access your location and personal information to ensure functionality." +
+                        " Do you allow us to use this data?")
+                .setPositiveButton("No", new DialogInterface.OnClickListener()
+                {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                })
+                .show();
 
 
         if (auth.getCurrentUser() != null) {
