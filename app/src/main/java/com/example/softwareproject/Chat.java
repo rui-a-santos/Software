866f2a1127844583a79913557bc48c5b6f60c006
Mat Slingsby <mgs128@hotmail.co.uk>
2019-05-13 03:40:24 +0100

    ui chng

--- app/src/main/java/com/example/softwareproject/Chat.java
+++ app/src/main/java/com/example/softwareproject/Chat.java
@@ -1,15 +1,17 @@
 package com.example.softwareproject;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
+import android.graphics.drawable.ColorDrawable;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
@@ -42,32 +44,9 @@ public class Chat extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.chat_options_menu, menu);
-
-        super.onCreateOptionsMenu(menu);
-        return true;
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if(item.getItemId()== R.id.new_chat_option){
-            Log.v("sup", user.getDisplayName());
-
-        }
-        if(item.getItemId()== R.id.settings_option){
-
-        }
-        if(item.getItemId()== R.id.settings_option){
-
-        }
 
 
-        return true;
 
 
-    }
+
 }
